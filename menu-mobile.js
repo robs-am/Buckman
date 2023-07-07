8c33c1ea211948f0aa0fe6609d1a65bbb73d69be
Roberta <roberta.amaro@ed3.com.br>
2023-07-07 16:24:57 -0300

    add nossas lojas e login tela 2 mobile

--- menu-mobile.js
+++ menu-mobile.js
@@ -18,6 +18,8 @@ const hideLogo = document.querySelector('[data-menu="logo"]');
 const hideSearch = document.querySelector('[data-menu="search-bar"]');
 const hideMinicart = document.querySelector('[data-menu="minicart"]');
 const closeButton = document.querySelector('[data-menu="mobile-close"]');
+const showLogin = document.querySelector('[data-menu="login"]');
+const showNossasLojas = document.querySelector('[data-menu="nossas-lojas"]');
 
 function toggleMenu() {
   menuList.classList.toggle("active");
@@ -26,13 +28,9 @@ function toggleMenu() {
   hideSearch.classList.toggle("active");
   hideMinicart.classList.toggle("active");
   closeButton.classList.toggle("active");
+  showLogin.classList.toggle("active");
+  showNossasLojas.classList.toggle("active");
 }
 
 menuButton.addEventListener("click", toggleMenu);
 closeButton.addEventListener("click", toggleMenu);
-
-const showDivion = document.querySelector("line-mobile");
-
-function showLineDivision() {
-  showDivion.classList.add("active");
-}
