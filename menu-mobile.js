98269991aa6349de8c6627f4e721f90961c00fae
Roberta <roberta.amaro@ed3.com.br>
2023-07-07 10:11:19 -0300

    add function openMenu

--- menu-mobile.js
+++ menu-mobile.js
@@ -1,1 +1,13 @@
-export default function initMenuMobile() {}
+/* export default function initMenuMobile() {} */
+
+const menuButton = document.querySelector('[data-menu="button"]');
+const menuList = document.querySelector('[data-menu="list"]');
+
+function openMenu(event) {
+  menuList.classList.add("active");
+  menuButton.classList.add("active");
+}
+
+menuButton.addEventListener("click", openMenu);
+
+openMenu();
