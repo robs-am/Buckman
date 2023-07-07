b3f86a75171dd091de26bee8bf80fbe846c10be4
Roberta <roberta.amaro@ed3.com.br>
2023-07-07 13:22:20 -0300

    add fucntion hide minicart

--- menu-mobile.js
+++ menu-mobile.js
@@ -16,12 +16,14 @@ const menuButton = document.querySelector('[data-menu="button"]');
 const menuList = document.querySelector('[data-menu="list"]');
 const hideLogo = document.querySelector('[data-menu="logo"]');
 const hideSearch = document.querySelector('[data-menu="search-bar"]');
+const hideMinicart = document.querySelector('[data-menu="minicart"]');
 
 function toggleMenu() {
   menuList.classList.toggle("active");
   menuButton.classList.toggle("active");
   hideLogo.classList.toggle("active");
   hideSearch.classList.toggle("active");
+  hideMinicart.classList.toggle("active");
 }
 
 menuButton.addEventListener("click", toggleMenu);
